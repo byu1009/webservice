aa0a36189dbc320f40ddb06705f621ec4be97744
marvelzmoon <61859027+marvelzmoon@users.noreply.github.com>
2025-11-19 13:38:56 +0700

    update Helper & Master Poli dan Dokter

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Auth;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Master\DokterController;
 use App\Http\Controllers\Master\PoliklinikController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -14,14 +16,17 @@ Route::get('/', function () {
 Route::post('/login', action: [AuthController::class, 'login']);
 Route::post('/auth/check-username', action: [AuthController::class, 'checkUsername']);
 Route::post('/check', action: [AuthController::class, 'check']);
-Route::get('/auth/check-username', action: [AuthController::class, 'test']);
+Route::get('/auth/login-data', action: [AuthController::class, 'loginData']);
 
 
-Route::get('/master/poliklinik', action: [PoliklinikController::class, 'index']);
-Route::post('/master/poliklinik', action: [PoliklinikController::class, 'store']);
+Route::post('/auth/test', action: [Auth::class, 'test']);
 
 Route::middleware(['api_token'])->group(function () {
     Route::controller(UserController::class)->group(function () {
         Route::get('/user', 'index');
     });
+    Route::get('/master/poliklinik', action: [PoliklinikController::class, 'index']);
+    Route::post('/master/poliklinik', action: [PoliklinikController::class, 'store']);
+    Route::get('/master/dokter', action: [DokterController::class, 'index']);
+    Route::post('/master/dokter', action: [DokterController::class, 'store']);
 });
